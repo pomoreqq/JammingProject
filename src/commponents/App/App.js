b1a7a90df581a2f102ee995634575779aca27dc0
pomoreqq <tomasz.pomorski99@gmail.com>
2024-07-25 08:44:45 +0200

    first commit

---
+++ src/commponents/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.module.css';
+import SearchBar from '../SearchBar/SearchBar'
+import SearchResult from '../SearchResult/SearchResult'
+import Playlist from '../Playlist/Playlist'
+
+function App() {
+  return (
+    <>
+    <SearchBar />
+    <SearchResult />
+    <Playlist />
+    </>
+  )
+}
+    
+     
+    
+  
+
+export default App;
